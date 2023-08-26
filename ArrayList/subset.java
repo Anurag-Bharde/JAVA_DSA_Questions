ccf6587665e0fbd8386c0b9d201b0cb7588e760b
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-08-26 23:47:25 +0530

    first

--- ArrayList/subset.java
+++ ArrayList/subset.java
@@ -3,15 +3,35 @@ package ArrayList;
 import java.util.*;
 
 public class subset {
-//    public static void main(String[] args) {
-//        int[] ar={1,2,3};
-//        subs(ar);
-//    }
-//    static List<List<Integer>> subs(int[] ar){
-//        if(){
-//
-//        }
-//        int r=ar[0];
-//
-//    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> subsets = generateSubsets(nums);
+
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+    public static List<List<Integer>> generateSubsets(int[] nums) {
+        return generateSubsetsHelper(nums, 0);
+    }
+
+    public static List<List<Integer>> generateSubsetsHelper(int[] nums, int index) {
+        if (index == nums.length) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(new ArrayList<>()); // Empty subset
+            return result;
+        }
+
+        List<List<Integer>> subsets = generateSubsetsHelper(nums, index + 1);
+        int current = nums[index];
+
+        List<List<Integer>> newSubsets = new ArrayList<>(subsets);
+        for (List<Integer> subset : subsets) {
+            List<Integer> newSubset = new ArrayList<>(subset);
+            newSubset.add(current);
+            newSubsets.add(newSubset);
+        }
+
+        return newSubsets;
+    }
 }
---
+++ StringBuffer/Intro.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class Intro {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer();
+        sb.append("allinone");
+        System.out.println(sb.capacity());
+        sb.append("aaara");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
