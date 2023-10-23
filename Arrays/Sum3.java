c44a0dda67ecefad2f957f1c0b91350bbf4fbe5a
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-10-23 18:49:51 +0530

    origgn

--- Arrays/Sum3.java
+++ Arrays/Sum3.java
@@ -23,8 +23,37 @@ public class Sum3 {
 
         System.out.println(pimp);
     }
+
+
+    static void optimal(int[] ar){
+       List<List<Integer>> listt=new ArrayList<>();
+       Arrays.sort(ar);
+        for(int i=0;i<ar.length;i++){
+            if(i!=0 && ar[i]==ar[i-1]) continue;
+            int j=i+1;
+            int k=ar.length-1;
+            while(j<k){
+                int sum=ar[i]+ar[j]+ar[k];
+                if (sum > 0) {
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    List<Integer> al=Arrays.asList(ar[i],ar[j],ar[k]);
+                    listt.add(al);
+                       j++; k--;
+                   while(j<k && ar[j]==ar[j-1]) j++;
+                   while(j<k && ar[k]==ar[k-1]) k--;
+
+                }
+            }
+        }
+        System.out.println(listt);
+    }
     public static void main(String[] args) {
         int[] ar={-1,0,1,2,-1,4};
-        summin(ar);
+        optimal(ar);
     }
 }
