ac967b358af8fdd5673bfeb86ae6e5436d99840b
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-11-14 23:04:08 +0530

    jkll

--- HashMap/SortChar.java
+++ HashMap/SortChar.java
@@ -2,6 +2,7 @@ package HashMap;
 import java.util.HashMap;
 
 public class SortChar {
+
     public static void main(String[] args) {
         String s="tree";
 
@@ -15,5 +16,20 @@ public class SortChar {
 
         }
         return s;
+    }
+    static int maxSubArraySum(int a[])
+    {
+        int size = a.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here
+                = 0;
+
+        for (int i = 0; i < size; i++) {
+            max_ending_here = max_ending_here + a[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+        return max_so_far;
     }
 }
